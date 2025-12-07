0f0b4e34c8d2d8ce899922ddd4613419ab7c6bc0
fsharpasharp <jnliuu@gmail.com>
2025-12-07 12:57:42 +0100

    Add day07

--- src/Solution.hs
+++ src/Solution.hs
@@ -1,6 +1,8 @@
 module Solution where
 
+import Data.Array (Array, bounds, (!))
 import Data.Text (Text)
+import Data.List (transpose)
 import Data.Void (Void)
 import Text.Megaparsec (Parsec, errorBundlePretty, parse)
 import Text.Megaparsec.Char (space)
@@ -16,3 +18,12 @@ parseOrDie p t =
 
 integer :: Parser Integer
 integer = L.signed space L.decimal
+
+prettyArrayChar :: Array (Int,Int) Char -> String
+prettyArrayChar arr =
+  let ((r0,c0),(r1,c1)) = bounds arr
+  in concat
+       [ [ arr ! (r,c) | c <- [c0..c1] ]
+         ++ (if r < r1 then "\n" else "")
+       | r <- [r0..r1]
+       ]
